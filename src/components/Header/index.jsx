498371b874eb2b948b1e8fb4a17b4cf34445c602
mehedi-hasan-anik <anikm2283@gmail.com>
2021-07-05 18:57:30 +0600

    addNavigation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
@@ -7,35 +9,63 @@ function Header() {
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <nav className="main-nav ">
-              <a href="index.html" className="logo">
-                Training<em> Studio</em>
-              </a>
-              <ul className="nav">
-                <li className="scroll-to-section">
-                  <a href="#top" className="active">
-                    Home
-                  </a>
-                </li>
-                <li className="scroll-to-section">
-                  <a href="#features">About</a>
-                </li>
-                <li className="scroll-to-section">
-                  <a href="#classes">classes</a>
-                </li>
-                <li className="scroll-to-section">
-                  <a href="#product">Product</a>
-                </li>
-                <li className="scroll-to-section">
-                  <a href="#contact-us">Contact</a>
-                </li>
-                <li className="main-button">
-                  <a href="/">Sign Up</a>
-                </li>
-              </ul>
-              <a className="menu-trigger" href="/">
-                <span>Menu</span>
-              </a>
+            <nav className="navbar navbar-expand-lg   container">
+              <div className="container-fluid nav-full-area">
+                <a className="navbar-brand" href="/">
+                  Training<em> Studio</em>
+                </a>
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon">
+                    <FontAwesomeIcon icon={faBars} className="nav-bar-icon" />
+                  </span>
+                </button>
+                <div
+                  className="collapse navbar-collapse navigation-area"
+                  id="navbarSupportedContent"
+                >
+                  <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                      <a href="/" className="nav-link ms-3">
+                        HOME
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="#features" className="nav-link ms-3">
+                        ABOUT
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="#classes" className="nav-link ms-3">
+                        CLASSES
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="#product" className="nav-link ms-3">
+                        {" "}
+                        PRODUCT
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="#contact" className="nav-link ms-3">
+                        CONTACT
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="#contact" className="nav-link ms-3">
+                        SIGN UP
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </nav>
           </div>
         </div>
