cbf9d9346ec00baaf00979a1da5652636fe946ea
mehedi-hasan-anik <anikm2283@gmail.com>
2021-07-06 17:07:31 +0600

    addReactHook

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import "./Form.css";
 
 function Form() {
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
-    <form id="contact" action="" method="post">
+    <form id="contact" onSubmit={handleSubmit(onSubmit)}>
       <div className="row">
         <div className="col-md-6 col-sm-12">
           <fieldset>
             <input
+              ref={register}
               name="name"
               type="text"
               id="name"
@@ -19,6 +28,7 @@ function Form() {
         <div className="col-md-6 col-sm-12">
           <fieldset>
             <input
+              ref={register}
               name="email"
               type="text"
               id="email"
@@ -31,6 +41,7 @@ function Form() {
         <div className="col-md-12 col-sm-12">
           <fieldset>
             <input
+              ref={register}
               name="subject"
               type="text"
               id="subject"
@@ -41,6 +52,7 @@ function Form() {
         <div className="col-lg-12">
           <fieldset>
             <textarea
+              ref={register}
               name="message"
               rows="6"
               id="message"
