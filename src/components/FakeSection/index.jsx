afca12bedb5cb0ff5c2855b602a9486bc539a0ad
mehedi-hasan-anik <anikm2283@gmail.com>
2021-07-06 16:54:30 +0600

    addNotFound

--- src/components/FakeSection/index.jsx
+++ src/components/FakeSection/index.jsx
@@ -5,6 +5,7 @@ import Loading from "../Loading";
 
 function FakeSection() {
   const [fakeData, setFakeData] = useState([]);
+  const [error, setError] = useState(false);
   const history = useHistory();
 
   const Handeler = (id) => {
@@ -17,35 +18,46 @@ function FakeSection() {
       .then((res) => res.json())
       .then((data) => {
         setFakeData(data.slice(0, 8));
+      })
+      .catch((error) => {
+        setError(true);
       });
   }, []);
 
   return (
     <div className="fake-section" id="product">
       <div className="container">
-        {fakeData.length ? (
-          <div className="row">
-            {fakeData.map((item) => (
-              <div
-                key={item.id}
-                className="col-md-3 p-4"
-                onClick={() => Handeler(item?.id)}
-              >
-                <div>
-                  <div className="fake-section-image">
-                    <img src={item?.image} className="img-fluid" alt="" />
-                  </div>
-                  <div className="fake-section-text">
-                    <h4>{item?.title}</h4>
-                    <p className="price">$ {item.price}</p>
-                    <p>{item?.description}</p>
-                  </div>
-                </div>
-              </div>
-            ))}
+        {error ? (
+          <div>
+            <h1 style={{ color: "white", textAlign: "center" }}>Not Found</h1>
           </div>
         ) : (
-          <Loading />
+          <>
+            {fakeData.length ? (
+              <div className="row">
+                {fakeData.map((item) => (
+                  <div
+                    key={item.id}
+                    className="col-md-3 p-4"
+                    onClick={() => Handeler(item?.id)}
+                  >
+                    <div>
+                      <div className="fake-section-image">
+                        <img src={item?.image} className="img-fluid" alt="" />
+                      </div>
+                      <div className="fake-section-text">
+                        <h4>{item?.title}</h4>
+                        <p className="price">$ {item.price}</p>
+                        <p>{item?.description}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <Loading />
+            )}
+          </>
         )}
       </div>
     </div>
